df25c537063eb59a9329a1e24df371104abbaee3
hpbeta <hpbeta16.l@gmail.com>
2025-02-11 15:05:06 -0300

    alteracoes

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,48 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
+const input = document.querySelector(".input");
+const swiperWrapper = document.querySelector(".swiper-wrapper");
 
-setInterval( function() {
-    nextImage();
-}, 5000)
+let swiper = new Swiper('.swiper', { 
+    loop: true,
+    autoplay: { delay: 3000 },
+    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+    pagination: { el: ".swiper-pagination", clickable: true },
+});
 
-function nextImage() {
-    count++;
-    if(count > 4) {
-        count = 1;
+input.addEventListener("change", (event) => {
+    const files = event.target.files;
+
+    if (files.length > 0) {
+        swiperWrapper.innerHTML = ""; 
+
+        Array.from(files).forEach((file, index) => {
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+               
+                const slide = document.createElement("div");
+                slide.classList.add("swiper-slide");
+
+                const img = document.createElement("img");
+                img.src = e.target.result;
+                img.alt = `Imagem ${index + 1}`;
+                slide.appendChild(img);
+                swiperWrapper.appendChild(slide);
+              
+                swiper.update();
+            };
+
+            reader.readAsDataURL(file);
+        });
+  
+        setTimeout(() => {
+            swiper.destroy(); 
+            swiper = new Swiper('.swiper', { 
+                loop: true,
+                autoplay: { delay: 3000 },
+                navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+                pagination: { el: ".swiper-pagination", clickable: true },
+            });
+        }, 100);
     }
-
-    document.getElementById("radio" + count).checked = true;
-}
-
-
-// const leitorDeArquivos = new FileReader();
-// leitorDeArquivos.readAsDataURL(imagemEnviada);
-// previaDaImagem.src = load.target.result;
+});
 
